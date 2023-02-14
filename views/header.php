09cbc2af24ab25317986f7a41725c5827ecaf3e8
Senpaixyz <jhicer@gmail.com>
2023-02-14 13:45:31 +0800

    updated jquery path

--- views/header.php
+++ views/header.php
@@ -47,7 +47,7 @@ $updateVersion = "?v=".rand();
 
     
 
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="./assets/javascripts/jquery-3.6.3.min.js"></script>
 
     <script>
         $(document).ready(function(){
